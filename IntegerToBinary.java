d06458dea72c99c83cf4c863389abc15dc64f67b
Vinay Chandra <vinaychandra3241@gmail.com>
2025-08-07 15:32:40 +0100

    Create IntegerToBinary.java

---
+++ IntegerToBinary.java
@@ -0,0 +1,12 @@
+class IntegerToBinary {
+public static void main(String []args){
+
+int num = 100;
+
+System.out.println("The Original Number Is : "+num);
+
+String binary = Integr.toBinaryString(num);
+
+}
+
+}
